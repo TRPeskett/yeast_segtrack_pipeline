c30b4a1dc604031481b4d12b532e5a09a6d0a929
Nadia Marounina <nmarounina@ethz.ch>
2024-03-05 10:34:54 +0100

    Removing unused files

--- input_parameters.py
+++
@@ -1,4 +0,0 @@
-
-path_to_full_movie="./input/example_movie/no_fluor.tif"
-path_to_template="./input/example_movie/template.tif"
-path_to_weights = './YeaZ-GUI/yeaz/unet/weights/weights_budding_BF_multilab_0_1'
